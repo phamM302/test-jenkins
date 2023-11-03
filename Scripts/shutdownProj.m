ba2ba4f65261656cf948da75dffe4d279511924b
phamM302 <phammichael1001@tamu.edu>
2023-11-03 05:51:14 -0500

    test

---
+++ Scripts/shutdownProj.m
@@ -0,0 +1,18 @@
+function shutdownProj
+% Clean up the environment for the current project.
+
+%% Reset location of slprj
+Simulink.fileGenControl('reset');
+
+%% Reset Simulation Data Inspector naming rule
+Simulink.sdi.setRunNamingRule('Run <run_index>: <model_name>');
+%% Clean things up
+clear
+clc
+%% Re-enable warnings
+warning('on','all')
+
+
+% Reset Simulation Data Inspector naming rule
+Simulink.sdi.setRunNamingRule('Run <run_index>: <model_name>');
+end
---
+++ Scripts/startupProj.m
@@ -0,0 +1,12 @@
+function startupProj
+    clear
+    % clear command window
+    clc
+    % turn off all warnings
+    warning('off','all')
+if ~batchStartupOptionUsed
+    %% Open instructions
+    edit Instructions.mlx
+end
+
+end
